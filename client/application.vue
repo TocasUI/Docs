1d11235532ea8adf42c5a38e24ce67ec7b34824b
Yami Odymel <yamiodymel@gmail.com>
2017-05-26 11:57:26 +0800

    added real sidebar for showcase

--- client/application.vue
+++ client/application.vue
@@ -7,8 +7,20 @@ body, html
     height: 100%
 </style>
 
-<template>
-    <router-view></router-view>
+<template lang="pug">
+    div
+        .ts.left.sidebar.inverted.vertical.menu
+            a.item(href="#!") 首頁
+            a.item(href="#!") 文件
+            a.item(href="#!") 部落格
+        .ts.left.sidebar
+            .ts.header
+                | 搭拉！
+                .sub.header 你打開了秘密的側邊欄！
+            .ts.segment
+                | 你可以像這樣在側邊欄放置很多不同的元件。
+        .pusher
+            router-view
 </template>
 
 <script>
