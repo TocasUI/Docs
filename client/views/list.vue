18f3c52e14555dbf4f93598db6797c69235d3b7f
Yami Odymel <yamiodymel@gmail.com>
2017-06-01 18:13:08 +0800

    dynmanic loading for list cards

--- client/views/list.vue
+++ client/views/list.vue
@@ -1,4 +1,9 @@
-<style lang="sass">
+<style lang="sass" module>
+.loader
+    height: calc(100vh - 45px - 212px)
+    display: flex
+    align-items: center
+    justify-content: center
 </style>
 
 <template lang="pug">
@@ -6,31 +11,47 @@
         //- 導航選單
         docs-navbar
 
+        //- 空白板岩
+        docs-slate(v-if="!docs.slate")
+            template(slot="header")      &nbsp;
+            template(slot="description") &nbsp;
+        //- 讀取指示器
+        div(:class="$style.loader", v-if="!docs.slate")
+            .ts.inline.big.loader.active
+
         //- 文件板岩
-        docs-slate
-            template(slot="header") {{ $route.meta.slate.title }}
-            template(slot="description") {{ $route.meta.slate.description }}
+        docs-slate(v-if="docs.slate")
+            template(slot="header")      {{ docs.slate.title }}
+            template(slot="description") {{ docs.slate.description }}
 
         //- 元素卡片
-        docs-cards(:items="$route.meta.items")
+        docs-cards(:items="docs.items")
 
         //- 頁腳
         docs-footer
 </template>
 
-<script>
+<script lang="coffee">
 import DocsSlate  from 'components/slate'
 import DocsNavbar from 'components/navbar'
 import DocsCards  from 'components/cards'
 import DocsFooter from 'components/footer'
+import store      from '../store'
 
-export default {
-    name: 'List',
+export default
+    name: 'List'
+    data: ->
+        docs: {}
+    created: ->
+        that = @
+        store.watch (state) ->
+            state.docs
+        , (docs) ->
+            that.docs = docs
     components: {
         DocsSlate,
         DocsNavbar,
         DocsCards,
         DocsFooter
     }
-}
 </script>
