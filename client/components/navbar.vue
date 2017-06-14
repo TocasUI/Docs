e4194f1c7793529a6da162f1cc4d0a0091180782
Yami Odymel <yamiodymel@gmail.com>
2017-06-14 13:38:52 +0800

    Changed import path to cdnjs

--- client/components/navbar.vue
+++ client/components/navbar.vue
@@ -4,6 +4,21 @@
 
     &[is-homepage]
         background-color: #87bdd1 !important
+
+.languageTag
+    transition: opacity 600ms cubic-bezier(0.77, 0, 0.175, 1)
+    opacity: 0
+    //display: none
+
+.languageTag[active]
+    display: inline
+    opacity: 1
+
+.languageDefault
+    transition: opacity 600ms cubic-bezier(0.77, 0, 0.175, 1)
+
+.languageDefault[disabled]
+    opacity: 0
 </style>
 
 <template lang="pug">
@@ -19,14 +34,26 @@
 
             //- 右側選單
             .right.menu
+                a.item
+                    span(:class="$style.languageDefault")
+                        i.globe.icon
+                        | Languages
                 a.item(href="//github.com/TeaMeow/TocasUI", target="_blank") GitHub
 </template>
 
-<script>
-export default {
-    name: 'Navbar',
-    props: {
-        isHomepage: { default: false }
-    }
-}
+<script lang="coffee">
+export default
+    name : 'Navbar'
+    props:
+        isHomepage:
+            default: false
+    mounted: ->
+        that = @
+        setTimeout ->
+            document.querySelector(".#{that.$style.languageDefault}").setAttribute("disabled", "disabled")
+            setTimeout ->
+                document.querySelector(".#{that.$style.languageDefault}").innerHTML = "<i class='globe icon'></i> 正體中文（台灣）"
+                document.querySelector(".#{that.$style.languageDefault}").removeAttribute("disabled")
+            , 600
+        , 1000
 </script>
--- client/views/index.vue
+++ client/views/index.vue
@@ -322,9 +322,9 @@ export default
         importCode:
             """
             <!-- Tocas UI：CSS 與元件 -->
-            <link rel="stylesheet" href="//cdn.rawgit.com/TeaMeow/TocasUI/2.3.2/dist/tocas.css">
+            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/tocas-ui/2.3.2/tocas.css">
             <!-- Tocas JS：模塊與 JavaScript 函式 -->
-            <script src="//cdn.rawgit.com/TeaMeow/TocasUI/2.3.2/dist/tocas.js"><\/script>
+            <script src="https://cdnjs.cloudflare.com/ajax/libs/tocas-ui/2.3.2/tocas.js"><\/script>
             """
         buttonExample:
             """
